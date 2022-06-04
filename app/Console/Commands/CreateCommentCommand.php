a1e488f16b8b6dd6ef95b8be1c2faea4703927d8
Jennifer <jeante05@gmail.com>
2022-06-04 14:26:06 -0500

    update class cretaCommetnCommand

--- app/Console/Commands/CreateCommentCommand.php
+++ app/Console/Commands/CreateCommentCommand.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Actions\Comment\CreateActions;
 use App\GetDataGateway\GetDataGatewayContract;
+use App\Repositories\Post\ColeccionsPostRepositories;
 
 class CreateCommentCommand extends Command
 {
@@ -13,22 +14,31 @@ class CreateCommentCommand extends Command
 
     protected $description = 'Command for create comment depend of id post random';
 
-    public function handle(GetDataGatewayContract $getDataGateway):int
+    //private ColeccionsPostRepositories $getPost;
+
+   /* public function __construct()
+    {
+        $this->getPostId = new ColeccionsPostRepositories;
+    }*/
+
+    public function handle(GetDataGatewayContract $getDataGateway, ColeccionsPostRepositories $getPostId):int
     {
         $this->url = "https://jsonplaceholder.typicode.com/comments";
         $comments = $getDataGateway->getData($this->url);
+        $idPost = $getPostId->getPostId();
 
         foreach($comments as $comment){
             $dataPost = [
                 'name' =>  $comment['name'],
                 'email' => $comment['email'] ,           
                 'body' => $comment['body'] ,  
-                'post_id' => $data['post_id']
+                'post_id' =>  $idPost
             ];
             
             $createPost = CreateActions::execute($dataPost);
+            return 1;
         }
         
-        return 1;
+       
     }
 }
