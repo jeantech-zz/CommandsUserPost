72de26dd5b68cca7ec6efe3825b897c3fef8a7e8
Jennifer <jeante05@gmail.com>
2022-06-04 09:16:56 -0500

    generation migration comments

---
+++ database/migrations/2022_06_04_141413_create_comments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up():void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->text('body');
+            $table->foreignId('post_id')->constrained('posts');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+};
