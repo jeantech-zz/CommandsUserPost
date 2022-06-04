ed761bae6b7c585973680a74c0a9a747632a3653
Jennifer <jeante05@gmail.com>
2022-06-03 22:02:08 -0500

    update retunr implement class GetDataGateway

--- app/GetDataGateway/GetDataGateway.php
+++ app/GetDataGateway/GetDataGateway.php
@@ -9,8 +9,7 @@ class GetDataGateway implements GetDataGatewayContract
     public function getData (string $url): json
     {
         $response = Http::get($url);
-        dd(json_decode($response->body(), true));
-       return json_decode($response->body(), true);
+        return json_decode($response->body(), true);
     }
 
 }
